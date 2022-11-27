cd3bb88da0316826b157808884bccc5a9c5c484f
DongGuk-Seo <failnxid@gmail.com>
2022-11-27 18:14:31 +0900

    feat : add refresh/verify view

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from .views import UserSignupView, UserCheckView, UserLoginView
+from rest_framework_simplejwt.views import TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('signup/',UserSignupView.as_view()),
     path('check/',UserCheckView.as_view()),
     path('login/',UserLoginView.as_view()),
+    path('refresh/',TokenRefreshView.as_view()),
+    path('verify/', TokenVerifyView.as_view()),
 ]
